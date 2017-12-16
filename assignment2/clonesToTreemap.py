fdf9340b56decbbf9662999ab1e54ac2630b0276
bicker <bickerkards@gmail.com>
2017-12-16 12:57:36 +0100

    treemap clone class now contain a file only once

--- assignment2/clonesToTreemap.py
+++ assignment2/clonesToTreemap.py
@@ -11,7 +11,7 @@ for line in f:
     if len(l) == 5:
         x = {'name': l[1], 'size': l[3], 'cloneTag': 'codeclone' + l[0]}
 
-        if l[0] in temp:
+        if l[0] in temp and l[1] not in [d['name'] for d in temp[l[0]]]:
             temp[l[0]].append(x)
         else:
             temp[l[0]] = [x]
